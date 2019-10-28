3751754d83b9d1487e35a27fe644d13b513282f5
Zoe Ali <zoeyzali@gmail.com>
2019-10-28 13:36:30 +0100

    content moved to index.js

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require( 'express' );
-
 const app = express();
-
+// Require the express module
+const path = require( 'path' );
+const Sass = require( './sass' );
+const config = require( './config.json' );
+const mongoose = require( 'mongoose' );
+const theRest = require( 'the.rest' );
 const port = 3000;
 
+
+
+// Connect to MongoDB via Mongoose
+mongoose.connect( 'mongodb+srv://dbAdmin:dbpassword@paywaydb-lsbwf.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, 
+console.log('db is up & running') );
+
+
+
+for ( let conf of config.sass ) {
+    new Sass( conf );
+}
+
+
+// ..and install the.rest as middleware
+// Arguments/configuration:
+// 1) The express library
+// 2) The base route for the REST api to create
+// 3) The path to a folder with mongoose-models
+//    Please Note: This path must be absolute
+const pathToModelFolder = path.join( __dirname, 'mongoose-models' );
+app.use( theRest( express, '/api', pathToModelFolder ) );
+
+app.use( express.static( 'public' ) );
 app.listen( port, () => {
-
     console.log( 'Server listening on', port );
-
 } );
