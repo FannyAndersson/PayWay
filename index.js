8cc0455136a8e659a973726acc44f7d0965f89f5
Eugenia <demichka@gmail.com>
2019-10-29 11:33:28 +0100

    added acl.js and acl-rules.json

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ for (let conf of config.sass) {
 	new Sass(conf);
 }
 
+// connect our own acl middleware
+const acl = require('./acl');
+const aclRules = require('./acl-rules.json');
+app.use(acl(aclRules));
+
 // ..and install the.rest as middleware
 // Arguments/configuration:
 // 1) The express library
@@ -31,6 +36,24 @@ for (let conf of config.sass) {
 //    Please Note: This path must be absolute
 const pathToModelFolder = path.join(__dirname, "mongoose-models");
 app.use(theRest(express, "/api", pathToModelFolder));
+
+// route to login
+app.post('/api/login', async (req, res) => {
+    let {name, password} = req.body;
+    // password = encryptPassword(password);
+    let user = await User.findOne({name, password})
+      .select('name role').exec();
+    if(user){ req.session.user = user };
+    res.json(user ? user : {error: 'not found'});
+  });
+  
+  // check if/which user that is logged in
+  app.get('/api/login', (req, res) => {
+    res.json(req.session.user ?
+      req.session.user :
+      {status: 'not logged in'}
+    );
+  });
 
 app.use(express.static("public"));
 app.listen(port, () => {
