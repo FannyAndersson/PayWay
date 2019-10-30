e42314c314f3db1f0f61edc98886e36e6ff7c2b4
Eugenia <demichka@gmail.com>
2019-10-30 23:20:42 +0100

    fixed test-update-user, checked if user is logged in, mongoose.connection saved as global.db

--- rest-queries/01-login.js
+++ rest-queries/01-login.js
@@ -1,11 +1,11 @@
 module.exports = ({ expect, response }) => ({
-    path: 'login',
-    method: 'post',
-    body: {
-        name: 'MagnusUggla',
-        password: '23456',
-    },
-    test() {
-        expect(response.name).to.equal('MagnusUggla');
-    }
-})
+	path: "login",
+	method: "post",
+	body: {
+		name: "Lennon",
+		password: "11111"
+	},
+	test() {
+		expect(response.name).to.equal("Lennon");
+	}
+});
