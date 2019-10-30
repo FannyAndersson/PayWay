9b951efd219435400ccdb7f27aae1483db17ead7
EliasHjelm <elias.hj@gmail.com>
2019-10-30 13:22:42 +0100

    changed queries

---
+++ rest-queries/02-get-logged-in-user.js
@@ -0,0 +1,8 @@
+// module.exports = ({ expect, response, assert }) => ({
+//     path: "login",
+//     method: "get",
+//     test() {
+//         // does the server accept login - in that case it returns user object
+//         expect(response.name).to.equal("MagnusUggla");
+//     }
+// });
