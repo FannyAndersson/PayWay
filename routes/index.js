bc3282ce7f2565b0375829798b77b5a66f3282c5
EliasHjelm <elias.hj@gmail.com>
2019-11-01 15:14:57 +0100

    feature: api endpoint to send money (create a transaction)

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 // import custom routes, add your custom routes here ...
 const exampleRoutes = require('./example-routes');
 const logout = require('./logout');
+const sendMoney = require('./send-money');
 
 const routesList = [];
 
 // ... and here
 routesList.push(exampleRoutes);
 routesList.push(logout);
+routesList.push(sendMoney);
 
 function useCustomRoutes(app, db) {
 
