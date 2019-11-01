e43a287b87867a6a25258ffccf752b5de11db085
Aleksandra <a.toholjevic@gmail.com>
2019-11-01 12:20:00 +0100

    added nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 // import custom routes, add your custom routes here ...
-const exampleRoutes = require('./example-routes');
-const logout = require('./logout');
+const exampleRoutes = require("./example-routes");
+const logout = require("./logout");
+const emailRoute = require("./email-route");
 
 const routesList = [];
 
 // ... and here
 routesList.push(exampleRoutes);
 routesList.push(logout);
+routesList.push(emailRoute);
 
 function useCustomRoutes(app, db) {
-
-    // tell express server to use routes
-    routesList.forEach(useRoute => {
-        useRoute(app, db);
-    });
-
-};
+  // tell express server to use routes
+  routesList.forEach(useRoute => {
+    useRoute(app, db);
+  });
+}
 
 module.exports = useCustomRoutes;
