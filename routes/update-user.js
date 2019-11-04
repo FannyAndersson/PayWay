cb769ac300846356bc0c439803ea3b71ee0b6401
Eugenia <demichka@gmail.com>
2019-11-04 15:08:34 +0100

    fixed user._id as string and admin access to update user

--- routes/update-user.js
+++ routes/update-user.js
@@ -3,9 +3,11 @@ const User = require("../mongoose-models/user.model");
 function updateUser(app) {
 	app.put("/api/profile/:id", async (req, res) => {
 		let user = await User.findOne({ _id: req.params.id });
-		// if (user === req.session.user) {
-		if (user) {
-			let result = await User.updateOne({ _id: req.params.id }, req.body);
+		if (
+			(user && String(user._id) === req.session.user._id) ||
+			req.session.user.role === "admin"
+		) {
+			await User.updateOne({ _id: req.params.id }, req.body);
 			res.json(await User.findOne({ _id: req.params.id }));
 		} else {
 			res.json({ error: "Another user logged in" });
