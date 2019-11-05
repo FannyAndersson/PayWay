7efb7683a2189bcfbac198029bfc4c0ea06a8cd3
Eugenia <demichka@gmail.com>
2019-11-05 11:48:06 +0100

    changed email address and added noreply text to subject

--- routes/reset-password.js
+++ routes/reset-password.js
@@ -1,87 +1,75 @@
-const User = require('../mongoose-models/user.model');
-const sendEmail = require('../send-email');
+const User = require("../mongoose-models/user.model");
+const sendEmail = require("../send-email");
 
 function resetPassword(app) {
+	// this is the route a user can call when they have forgotten their password
+	// and wish to get a link they can click to get a new password
+	app.post("/api/reset-password", async (req, res) => {
+		// get mail from request body
+		const { email } = req.body;
 
-    // this is the route a user can call when they have forgotten their password
-    // and wish to get a link they can click to get a new password
-    app.post('/api/reset-password', async (req, res) => {
+		// find user with email
+		const user = await User.findOne({ email });
 
-        // get mail from request body
-        const { email } = req.body;
+		// if no user, notify the frontend
+		if (!user) {
+			return res.status(404).send("no such user");
+		}
 
-        // find user with email
-        const user = await User.findOne({ email });
+		// generate the users reset link (to the endpoint we create below)
+		const link = `http://localhost:3000/api/reset-password/${user._id}`;
 
-        // if no user, notify the frontend
-        if (!user) {
-            return res.status(404).send('no such user');
-        }
+		// send email to the user with the password reset link
+		sendEmail({
+			to: user.email,
+			html: `<body><p>Click this link to get a new assword and a free iPad - ${link}</p></body>`,
+			subject: "PayWay - Reset Password NO REPLY"
+		});
 
-        // generate the users reset link (to the endpoint we create below)
-        const link = `http://localhost:3000/api/reset-password/${user._id}`;
+		// respond with status 200 (ok) to let frontend know that it was successful
+		res.status(200).end();
+	});
 
-        // send email to the user with the password reset link
-        sendEmail({
-            to: user.email,
-            html: `<body><p>Click this link to get a new assword and a free iPad - ${link}</p></body>`,
-            subject: 'PayWay - Reset Password',
-        });
+	// this is the link that is sent out
+	// when this 'endpoint' is 'hit' the users password should be set
+	// to something random, and an email with this new password
+	// send to the user
+	app.get("/api/reset-password/:id", async (req, res) => {
+		// get a password (it's not really unique, but it should be 'unique enough' for our purposes)
+		const password = generateUniquePassword();
 
-        // respond with status 200 (ok) to let frontend know that it was successful
-        res.status(200).end();
+		try {
+			// get the user
+			const user = await User.findById(req.params.id);
 
-    });
+			// change the password
+			user.password = password;
+			await user.save();
 
-    // this is the link that is sent out
-    // when this 'endpoint' is 'hit' the users password should be set
-    // to something random, and an email with this new password
-    // send to the user
-    app.get('/api/reset-password/:id', async (req, res) => {
+			// notify the user with mail
+			sendMailWithNewPassword(user.email, password);
 
-        // get a password (it's not really unique, but it should be 'unique enough' for our purposes)
-        const password = generateUniquePassword();
-
-        try {
-
-            // get the user
-            const user = await User.findById(req.params.id);
-
-            // change the password
-            user.password = password;
-            await user.save();
-
-            // notify the user with mail
-            sendMailWithNewPassword(user.email, password);
-
-            res.send('We have sent an email to you containing your new assword. You can use this assword to login.');
-
-        } catch (error) {
-
-            // respond with status 500 (internal server error) to let frontend know there was an error when processing
-            // the request. Also send error obj as response.
-            res.status(500).json(error);
-
-        }
-
-    });
-
+			res.send(
+				"We have sent an email to you containing your new assword. You can use this assword to login."
+			);
+		} catch (error) {
+			// respond with status 500 (internal server error) to let frontend know there was an error when processing
+			// the request. Also send error obj as response.
+			res.status(500).json(error);
+		}
+	});
 }
 
 function generateUniquePassword() {
-
-    return String(Math.random());
-
+	return String(Math.random());
 }
 
 function sendMailWithNewPassword(email, password) {
-
-    sendEmail({
-        to: email,
-        subject: 'PayWay - New password',
-        html: `<body><p>Hi! Your new password is ${password}. You can now user your new password to log into PayWay. We recommend that you change this password to something more fun.`,
-    });
-
+	sendEmail({
+		to: email,
+		subject: "PayWay - New password",
+		html: `<body><p>Hi! Your new password is ${password}. You can now user your new password to log into PayWay. We recommend that you change this password to something more fun.`
+	});
 }
 
 module.exports = resetPassword;
--- send-email.js
+++ send-email.js
@@ -2,46 +2,43 @@ const nodemailer = require("nodemailer");
 const { mail } = require("./config/config");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.sendgrid.net",
-    port: 465,
-    secure: true,
-    auth: {
-        user: "apikey",
-        pass: mail
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
+	host: "smtp.sendgrid.net",
+	port: 465,
+	secure: true,
+	auth: {
+		user: "apikey",
+		pass: mail
+	},
+	tls: {
+		rejectUnauthorized: false
+	}
 });
 
 function sendEmail(options) {
+	const defaultMailOptions = {
+		from: `"PayWay Notification NOREPLY" demichka@gmail.com`,
+		subject: "Email subject",
+		html:
+			"<body><h3>Hello Zhenya!</h3><p>Hallå Zhenya! Hur är det med dig?</p></body>",
+		to: null
+	};
 
-    const defaultMailOptions = {
-        from: `"PayWay" payway@gmail.com`,
-        subject: "Email subject",
-        html:
-            "<body><h3>Hello Zhenya!</h3><p>Hallå Zhenya! Hur är det med dig?</p></body>",
-        to: null,
-    };
+	const mailOptions = {
+		...defaultMailOptions,
+		...options
+	};
 
-    const mailOptions = {
-        ...defaultMailOptions,
-        ...options,
-    };
+	if (!mailOptions.to) {
+		throw new Error("Mail must have recipient ('to' property)");
+	}
 
-    if (!mailOptions.to) {
-
-        throw new Error('Mail must have recipient (\'to\' property)');
-
-    }
-
-    transporter.sendMail(mailOptions, function(err, res) {
-        if (err) {
-            console.error("there was an error: ", err);
-        } else {
-            console.log("here is the res: ", res);
-        }
-    });
+	transporter.sendMail(mailOptions, function(err, res) {
+		if (err) {
+			console.error("there was an error: ", err);
+		} else {
+			console.log("here is the res: ", res);
+		}
+	});
 }
 
 module.exports = sendEmail;
