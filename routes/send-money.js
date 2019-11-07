8ebbe0bcd1c8b0a525f3c93e02aa4324d6826b5f
Fanny <fanny_andersson@hotmail.com>
2019-11-07 15:08:39 +0100

    Decrease senders money after transaction

--- routes/send-money.js
+++ routes/send-money.js
@@ -2,20 +2,15 @@ const Transaction = require('../mongoose-models/transaction.model');
 const User = require('../mongoose-models/user.model');
 
 function sendMoney(app) {
-
-    app.post('/api/send-money', async (req, res) => {
-
+    app.post('/api/send-money', async(req, res) => {
         const { user } = req.session;
 
         if (!user) {
-
             // return statement just to terminate function, only calling send() doesn't stop rest of function from executing
             return res.status(400).send('You must be logged in to send money!');
-
         }
 
         try {
-
             // get sender id from the users session
             const senderId = user._id;
 
@@ -27,45 +22,47 @@ function sendMoney(app) {
 
             // this shouldn't happen, but it might
             if (!sender) {
-
                 return res.status(500).send('sender does not exist in db');
-
             }
 
             // check that sender has enogugh money
             if (sender.balance < amount) {
-
                 return res.status(400).send('You do not have enough money!');
-
             }
 
             // convert phone nr to string just in case
-            const actualRecipient = await User.findOne({ 'phone': String(recipient) });
+            const actualRecipient = await User.findOne({ phone: String(recipient) });
 
             if (!actualRecipient) {
-
                 return res.status(404).send('No such user');
-
             }
+
+            let b = sender.balance;
+
+            console.log(b, 'bbb');
 
             // show me the money
             const transaction = new Transaction({
                 recipient: actualRecipient._id,
                 amount,
                 message,
-                sender,
+                sender
             });
+
+            // decrease senders money on account
+            let remainingTransactions =
+                transaction.sender.balance - transaction.amount;
+
+            sender.balance = remainingTransactions;
+
+            const newBalance = await sender.save();
 
             const result = await transaction.save();
 
             res.json(result);
-
         } catch (error) {
-
             res.status(500).json(error);
-
         }
-
     });
 }
 
