f153f8f2930d7e5e4e5fd529496f5a5291657d9d
Zoe Ali <zoeyzali@gmail.com>
2019-11-08 17:41:46 +0100

    fixed: only loggedIn user can access its transactions

--- routes/user-transactions.js
+++ routes/user-transactions.js
@@ -4,17 +4,27 @@ const User = require('../mongoose-models/user.model');
 function getUserTransactions(app){
     app.get('/api/users/:id/transactions', async(req, res)=>{
 const {user} = await req.session;
-if(user !== req.session.user){
-    return res.status(400).json('You are not logged in');
+if(!user){
+    return res.status(400).json('Make sure to login, please');
 }
-const myMonies = await User.findById(req.params.id).populate('incomingTransactions').populate('outgoingTransactions');
-// console.log(myMonies, 'making money move')
+try{
+    const myMonies = await User.findById(req.params.id).populate('incomingTransactions').populate('outgoingTransactions');
+    // console.log(myMonies, 'making money move')
+
+    if(user._id !== req.params.id){
+        return res.status(400).json('Are you sure you are logged in?')
+    } else {
         res.json({
             incomingTransactions: myMonies.incomingTransactions, 
             outgoingTransactions: myMonies.outgoingTransactions, 
             message: 'making money move'})
+    }
+} catch(error){
+    res.status(500).send(error)
+}
     })
 }
+
 
 
 module.exports = getUserTransactions;
