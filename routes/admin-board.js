4f68a7adacd928d80e328e1d06368e56fea34824
Zoe Ali <zoeyzali@gmail.com>
2019-11-11 13:18:15 +0100

    modified access routes

--- routes/admin-board.js
+++ routes/admin-board.js
@@ -4,7 +4,7 @@ const Transaction = require('../mongoose-models/transaction.model')
 function adminBoard (app){
 app.get('/api/admin-dashboard', async (req, res)=>{
 const user = await req.session.user
-if(user && user.role !== "admin"){
+if(user.role !== "admin"){
     res.status(400).json('whatcha looking for boi?')
 }
 if(user.role === "admin"){
@@ -12,7 +12,7 @@ if(user.role === "admin"){
     // let transactions= await Transaction.find()
     res.json({users, message: 'Boo yeah'})
 }
-console.log(user.role, user.name, 'Admin Bae')
+console.log(user.role, user.name, 'bae')
 })
 }
 
