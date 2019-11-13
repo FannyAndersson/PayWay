64d978af8df9087dda43a3f4bab572a6bde759bd
EliasHjelm <elias.hj@gmail.com>
2019-11-13 14:59:11 +0100

    added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <h2>Payway</h2>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2>Payway</h2>
+
+            <Route path="/hej">
+                Hej!
+            </Route>
+        </div>
+    );
 }
 
 export default App;
