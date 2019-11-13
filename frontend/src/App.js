8c081b4afe4c1ec5087ca210b17a63b92f04edbb
Eugenia <demichka@gmail.com>
2019-11-14 00:12:40 +0100

    added login form, header, context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import './App.css';
+import {UserContext} from './userContext';
+import './css/style.css';
 import MainPage from './components/MainPage/MainPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import Header from './components/Header/Header';
 
 function App() {
+	const checkLogin = () => {
+		return fetch('/api/login').then(res => {return res.json()});
+	}
+	const [user, setUser] = useState('');
+	checkLogin().then(res => {if(res) {setUser(res)}});
+	
   return (
-
-	<Router>
-		<Switch>
-          <Route path="/">
-            <MainPage />
-          </Route>
-        </Switch>
-	</Router>
+	  <UserContext.Provider value={user}>
+			<React.Fragment>
+				<section className={'container-fluid'}>
+				<Header />
+				<Router>
+					<Switch>
+						<Route exact path="/">
+						<MainPage />
+						</Route>
+						<Route exact path="/login">
+							<LoginPage />
+						</Route>
+					</Switch>
+				</Router>
+				</section>
+			</React.Fragment>
+	  </UserContext.Provider>
+	
+	
   );
 }
 
