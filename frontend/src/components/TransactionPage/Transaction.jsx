4854e618ab5e071589d6d4619a82e90096c449ee
Aleksandra <a.toholjevic@gmail.com>
2019-11-13 19:29:43 +0100

    fixes

--- frontend/src/components/TransactionPage/Transaction.jsx
+++ frontend/src/components/TransactionPage/Transaction.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Transaction = () => {
-  return (
-    <div className="transaction">
-      <h2>Transaction</h2>
-    </div>
-  );
+    return (
+        <div className="transaction">
+            <h2>Transaction</h2>
+        </div>
+    );
 };
 
 export default Transaction;
--- frontend/src/components/TransactionPage/TransactionPage.jsx
+++ frontend/src/components/TransactionPage/TransactionPage.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import Transaction from "./Transaction";
 
 const TransactionPage = () => {
-  return (
-    <div className="container">
-      <h1>Transactions</h1>
-      <p>All, incoming and outgoing</p>
-      <button>All</button>
-      <button>Incoming</button>
-      <button>Outgoing</button>
+    return (
+        <div className="container">
+            <h1>Transactions</h1>
+            <p>All, incoming and outgoing</p>
+            <button>All</button>
+            <button>Incoming</button>
+            <button>Outgoing</button>
 
-      <Transaction />
-    </div>
-  );
+            <Transaction />
+        </div>
+    );
 };
 
 export default TransactionPage;
