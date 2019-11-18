6bab9c9c831bf20b0097ba351f5fe8e840a9fddc
Pablo44 <pawelrz@wp.pl>
2019-11-18 15:52:40 +0100

    button to favourites

--- frontend/src/components/favouritesComponent/Favourites.jsx
+++ frontend/src/components/favouritesComponent/Favourites.jsx
@@ -11,18 +11,15 @@ const Favourites = () => {
                 user => (
                     <React.Fragment>
                         <Contact />
-                        <ul>
-                            <li>
-                                Zenya 222
-                            </li>
-                            <li>
-                                Alexandra 333
-                            </li>
-                        </ul>
-                        <TextInput className="form-control" label="Email" email={true} s={12} l={12} required />
-                        <TextInput className="form-control" label="Password" password={true} s={12} l={12} required /><Button className="login-btn" waves="light" style={{ width: '100%' }} type="button">
-                            Add
-                        </Button></React.Fragment>
+                        
+                        <TextInput className="form-control" label="Phone" phone={true} required />
+                        <Button type ="submit" waves ='light'>
+                            Subbmit
+                            <Icon right>
+                                send
+                                </Icon>
+                        </Button>
+                        </React.Fragment>
 
 
                 )
