9f6464f0ff8dec18ff160e42dedde7a1d25ed9c1
Pablo44 <pawelrz@wp.pl>
2019-11-18 15:21:48 +0100

    little space btw rows

--- routes/createFavorite.js
+++ routes/createFavorite.js
@@ -4,6 +4,11 @@ function createFavorite(app, db) {
     app.post('/api/createFavorite', async (req, res) => {
 
         const { user } = req.session;
+
+        if(!user){
+            return res.status(404).send({error:"you are not logged in"})
+        }
+
         const currentUser = await User.findById(user._id);
 
 
@@ -19,7 +24,12 @@ function createFavorite(app, db) {
         if (!favoriteUser) {
             return res.status(404).json({ error: "No such user" });
         }
-
+        
+        if(currentUser){
+            if(String(favoriteUser._id === req.params.id)){
+                return res.status(404).send('You can not be a favourite to yourself!')
+            }
+        }
         //checking if user exists in favorites
         if (currentUser.favorites.includes(favoriteUser._id)) {
             res.send("Request has already been sent to this user")
@@ -32,11 +42,7 @@ function createFavorite(app, db) {
 
         }
 
-        if(currentUser){
-            if(String(favoriteUser._id ===req.params.id)){
-                return res.status(404).send('You can not be a favourite to yourself!')
-            }
-        }
+        
 
     })
 }
