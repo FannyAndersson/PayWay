1768f83f065245a1a11451aa2adfaf2c8934e5ac
Pablo44 <pawelrz@wp.pl>
2019-11-18 16:42:15 +0100

    addFav() function and list form in one component

--- frontend/src/components/favouritesComponent/Favourites.jsx
+++ frontend/src/components/favouritesComponent/Favourites.jsx
@@ -5,15 +5,25 @@ import { Row, Col, TextInput, Button } from 'react-materialize';
 
 const Favourites = () => {
 
+    const addFav = () => {
+       setUser([...favourites, user._id]);
+    }
+    
     return (
         <UserContext.Consumer>
             {
                 user => (
                     <React.Fragment>
-                        <Contact />
+                        <div className= 'fav-list'>
+                            <ul>
+                                {favourites.map(user_id =>{
+                                    return (<li key={user_id}>{Favourites[i]}</li>)
+                                })}
+                            </ul>
+                        </div>
                         
                         <TextInput className="form-control" label="Phone" phone={true} required />
-                        <Button type ="submit" waves ='light'>
+                        <Button type ="submit" onclick= {addFav} waves ='light'>
                             Subbmit
                             <Icon right>
                                 send
