320914a9c298fa0f60cae8325349f2c34a06cd2c
Eugenia <demichka@gmail.com>
2019-11-19 10:08:08 +0100

    fixed attempt of self-favorite

--- routes/createFavorite.js
+++ routes/createFavorite.js
@@ -10,25 +10,18 @@ function createFavorite(app, db) {
         }
 
         const currentUser = await User.findById(user._id);
-
-
         const favoriteUser = await User.findOne({ phone: req.body.phone })
-
-
 
         if (!currentUser) {
             return res.status(404).json({ error: "User not found" });
         }
 
-
         if (!favoriteUser) {
-            return res.status(404).json({ error: "No such user" });
+            return res.status(404).json({ error: "No such user with this phone number" });
         }
         
-        if(currentUser){
-            if(String(favoriteUser._id === req.params.id)){
-                return res.status(404).send('You can not be a favourite to yourself!')
-            }
+        if(String(favoriteUser._id) === user._id){
+            return res.status(404).send('You can not be a favourite to yourself!')
         }
         //checking if user exists in favorites
         if (currentUser.favorites.includes(favoriteUser._id)) {
@@ -41,8 +34,6 @@ function createFavorite(app, db) {
             res.status(200).end();
 
         }
-
-        
 
     })
 }
