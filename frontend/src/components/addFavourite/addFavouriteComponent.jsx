171c0f4cd025607a7cc5ab856d3683a46f9766bb
Pablo44 <pawelrz@wp.pl>
2019-11-19 17:08:24 +0100

    scheme for onChange andonSubmit func into custom hook

--- frontend/src/components/addFavourite/addFavouriteComponent.jsx
+++ frontend/src/components/addFavourite/addFavouriteComponent.jsx
@@ -13,18 +13,21 @@ const {inputs, handleInputChange, handleSubmit} = useAddFavourites();
 
     return (
             <React.Fragment>
-
+                <form onSubmit={handleSubmit}>
             <Row>
                 <Col>
                 <h1>Add your favourite</h1>
-                <TextInput/>
                 
+                <TextInput type='text' lable ='Phone' name='phone' onChange={handleInputChange} value={inputs.phone} phone={true} required/>     
+                <Button >
+                    Send
+                    </Button>         
                 
                 </Col>
 
 
             </Row>
-
+                </form>
 
             </React.Fragment>
 
