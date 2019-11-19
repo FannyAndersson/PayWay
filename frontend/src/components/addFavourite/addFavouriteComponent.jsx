4c0c1f593e97f549ff9f9e51992737e5ab368702
Pablo44 <pawelrz@wp.pl>
2019-11-19 15:32:48 +0100

    add favourite component frame

---
+++ frontend/src/components/addFavourite/addFavouriteComponent.jsx
@@ -0,0 +1,32 @@
+import React, {useContext} from 'react';
+import {Row, Column, TextInput, Button} from 'react';
+import { UserContext } from '../../AuthUserContext';
+
+const {fav,SetFav} = useContext(UserContext);
+
+
+const addFavFavouriteComponent= () => {
+
+
+
+
+
+
+    return (
+            <React.Fragment>
+
+
+
+
+
+                
+            </React.Fragment>
+
+
+
+
+    );
+}
+
+export default addFavFavouriteComponent;
+
