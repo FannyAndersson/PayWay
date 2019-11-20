503c5e580dbd4098ba9825276ed21668165c78fd
Pablo44 <pawelrz@wp.pl>
2019-11-20 12:00:40 +0100

    promise to handle add favoirites to the user

--- frontend/src/components/addFavourite/addFavouriteComponent.jsx
+++ frontend/src/components/addFavourite/addFavouriteComponent.jsx
@@ -1,38 +1,52 @@
-import React, {useContext} from 'react';
-import {Row, Column, TextInput, Button} from 'react';
+import React, { useContext } from 'react';
+import { Row, Column, TextInput, Button } from 'react';
 import { UserContext } from '../../AuthUserContext';
 import useAddFavourites from './useAddFavouriteHook';
 
 
-const addFavFavouriteComponent= () => {
+const addFavFavouriteComponent = () => {
+
+    try {
+        const input = {
+            phone: inputs.phone
+        }
+        const resp = await fetch('/api/favourites', {
+            method: 'POST',
+            body: JSON.stringify(input),
+            headers: {
+                'Content-Type': 'aplication/json'
+            }
+
+        });
+    }
+    catch(error){
+        console.error('Error:',error)
+    }
+    
+    
 
 
-
-
-const {inputs, handleInputChange, handleSubmit} = useAddFavourites();
+    const { inputs, handleInputChange, handleSubmit } = useAddFavourites();
 
     return (
-            <React.Fragment>
-                <form onSubmit={handleSubmit}>
-            <Row>
-                <Col>
-                <h1>Add your favourite</h1>
-                
-                <TextInput type='text' lable ='Phone' name='phone' onChange={handleInputChange} value={inputs.phone} phone={true} required/>     
-                <Button >
-                    Send
-                    </Button>         
-                
-                </Col>
+        <React.Fragment>
+            <form onSubmit={handleSubmit}>
+                <Row>
+                    <Col>
+                        <h1>Add your favourite</h1>
+
+                        <TextInput type='text' lable='Phone' name='phone' onChange={handleInputChange} value={inputs.phone} phone={true} required />
+                        <Button >
+                            Send
+                    </Button>
+
+                    </Col>
 
 
-            </Row>
-                </form>
+                </Row>
+            </form>
 
-            </React.Fragment>
-
-
-
+        </React.Fragment>
 
     );
 }
