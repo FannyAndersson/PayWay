8a5e45a6a00f58e5f8f675c79fbbdb4cf6e9d01f
Fanny <fanny_andersson@hotmail.com>
2019-11-20 22:37:21 +0100

    Add modal to add child

--- frontend/src/components/AddChildToParent/AddChildForm.jsx
+++ frontend/src/components/AddChildToParent/AddChildForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col, TextInput, Button } from 'react-materialize';
+import { Row, Col, TextInput, Button, Modal } from 'react-materialize';
 import useAddChild from './useAddChild';
 
 const AddChildForm = () => {
@@ -19,6 +19,8 @@ const AddChildForm = () => {
             })
                  if (response.ok) {
                    console.log(response, 'SUCCESS');
+                 } else {
+                   console.log('weee')
                  }
 
             }   catch (error) {
@@ -29,18 +31,24 @@ const AddChildForm = () => {
         const { inputs, handleInputChange, handleSubmit } = useAddChild(addChild);
 
 
+
     return <React.Fragment>
         <Row>
           <Col l={3} offset="l4" className="content">
             <h1>Add a child</h1>
-            <Col node="form" onSubmit={handleSubmit} l={12} className="form">
+            <Col node="form" l={12} className="form">
               <TextInput className="form-control" name="phone" onChange={handleInputChange} style={{ marginBottom: '40px' }} value={inputs.phone} label="Phone number" s={12} l={12} required />
               <Button flat={true} className="cancel-add-child-btn raised-btn" style={{ width: '48%' }} waves="light">
                 <Link to="/mainpage">CANCEL</Link>
               </Button>
-              <Button className="send-add-child-btn" waves="light" style={{ width: '48%', marginLeft: '11px' }}>
-                SEND
-              </Button>
+              <Modal header="Modal Header" trigger={<Button className="send-add-child-btn" waves="light" style={{ width: '48%', marginLeft: '11px' }}>
+                    SEND
+                  </Button>}>
+                <p>Are you sure you want to continue?</p>
+                <Button flat={true} onClick={handleSubmit} className="send-add-child-btn raised-btn" style={{ width: '48%' }} waves="light">
+                  SEND
+                </Button>
+              </Modal>
             </Col>
           </Col>
         </Row>
