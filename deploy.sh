4f98da4665828ad625ee7867f31ce4f87d12f101
EliasHjelm <elias.hj@gmail.com>
2019-11-21 15:24:10 +0100

    adjusted deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
+# attempt automatic deployment
 echo "Starting automatic deployment... please take a deep breath :)"
 
+pm2 stop paywayapp
 cd /var/www/PayWay
 git fetch
 git checkout master
 git pull
-pm2 restart paywayapp
+pm2 start paywayapp
 
 echo "Automatic deploy finished! Let's crack open a cold beer and enjoy this moment!"
