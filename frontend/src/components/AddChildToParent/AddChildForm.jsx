fd21c0708cc1f05630596238308d5111d4761ae1
Fanny <fanny_andersson@hotmail.com>
2019-11-21 23:26:23 +0100

    Validate phone number add child

--- frontend/src/components/AddChildToParent/AddChildForm.jsx
+++ frontend/src/components/AddChildToParent/AddChildForm.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, TextInput, Button, Modal } from 'react-materialize';
 import useAddChild from './useAddChild';
@@ -7,7 +7,9 @@ import useAddChild from './useAddChild';
 const AddChildForm = () => {
 
 
+
     const addChild = async () => {
+
         try {
             const add = {
                 phone: inputs.phone
@@ -20,9 +22,11 @@ const AddChildForm = () => {
                   }
             })
                  if (response.ok) {
+                   setShowInvalidChild(false);
                    console.log(response, 'SUCCESS');
-                 } else {
-                   console.log('weee')
+                 } else{
+                   console.log('wee')
+                   setShowInvalidChild(true);
                  }
 
             }   catch (error) {
@@ -30,7 +34,10 @@ const AddChildForm = () => {
         }
         }
 
-        const { inputs, handleInputChange, handleSubmit } = useAddChild(addChild);
+
+
+
+        const { inputs, handleInputChange, handleSubmit, setShowInvalidChild, showInvalidChild } = useAddChild(addChild);
 
 
     return <React.Fragment>
@@ -38,50 +45,26 @@ const AddChildForm = () => {
           <Col l={3} offset="l4" className="content">
             <h1>Add a child</h1>
             <Col node="form" l={12} className="form">
-              <TextInput name="phone" onChange={handleInputChange} style={{ marginBottom: '40px' }} value={inputs.phone} label="Phone number" s={12} l={12} required />
+              <TextInput
+               name="phone"
+               onChange={handleInputChange}
+                style={{ marginBottom: '40px' }}
+                 value={inputs.phone}
+                 className={ `validate${showInvalidChild ? 'invalid' : ''}`}
+                  label="Phone number"
+                   s={12} l={12}
+                   required />
+                   {showInvalidChild ? (
+              <p style={{ color: 'red' }}>There is no recipient with this phone number</p>
+            ) : (
+              ''
+            )}
               <Button flat={true} className="cancel-add-child-btn raised-btn" style={{ width: '48%' }} waves="light">
                 <Link to="/mainpage">CANCEL</Link>
               </Button>
-              <Modal className="modal-send" actions={<p />} trigger={<Button className="send-add-child-btn" waves="light" style={{ width: '48%', marginLeft: '11px' }}>
-                    ADD CHILD
-                  </Button>}>
-                <p
-                  style={{
-                    fontSize: '17px',
-                    width: '100%',
-                    marginBottom: '28px',
-                    padding: '16px'
-                  }}
-                >
-                  A mail is going to be sent to your child who has to
-                  confirm you as a parent. Are you sure you want to
-                  continue?
-                </p>
-                <Modal actions={<p />} trigger={<Button flat={true} onMouseDown={handleSubmit} modal="close" className="send-add-child-btn" style={{ width: '48%', backgroundColor: '#6200EE', color: 'white' }} waves="light">
-                      SEND
-                    </Button>}>
-                  <p
-                    style={{
-                      fontSize: '17px',
-                      width: '100%',
-                      marginBottom: '28px',
-                      padding: '16px'
-                    }}
-                  >
-                    Your question has been sent. Do you want to add another
-                    child or you want go back to your profile?
-                  </p>
-                  <Button flat={true} className="send-add-child-btn" modal="close" style={{ width: '48%', backgroundColor: '#6200EE', color: 'white' }} waves="light">
-                    ADD CHILD
-                  </Button>
-                  <Button flat={true} className="send-add-child-btn" style={{ width: '48%', backgroundColor: '#fffff', border: 'solid', borderWidth: '0.6px', borderColor: '#6200EE', color: '#6200EE', marginLeft: '7px' }} waves="light">
-                    <Link to="/mainpage">PROFILE</Link>
-                  </Button>
-                </Modal>
-                <Button flat={true} className="send-add-child-btn" modal="close" style={{ width: '48%', backgroundColor: '#fffff', border: 'solid', borderWidth: '0.6px', borderColor: '#6200EE', color: '#6200EE', marginLeft: '7px' }} waves="light">
-                 CANCEL
-                </Button>
-              </Modal>
+                <Button flat={true} onClick={handleSubmit} className="send-add-child-btn" style={{ width: '48%', backgroundColor: '#6200EE', color: 'white', marginLeft:'11px' }} waves="light">
+                      ADD CHILD
+                    </Button>
             </Col>
           </Col>
         </Row>
--- frontend/src/components/AddChildToParent/useAddChild.jsx
+++ frontend/src/components/AddChildToParent/useAddChild.jsx
@@ -5,29 +5,33 @@ const useAddChild = (callback) => {
 
     const[ inputs, setInputs ] = useState({ phone: '' })
 
+    const [showInvalidChild, setShowInvalidChild] = useState(false);
+
 
 
     const handleSubmit = (event) => {
         if(event){
             event.preventDefault()
 
+
         }
+
         callback()
     }
 
     const handleInputChange = (event) => {
         event.persist()
 
+
         setInputs(inputs => (
             {...inputs, [event.target.name]: event.target.value}
         ))
+        setShowInvalidChild(false);
     }
 
     return {
-        inputs,
-        handleSubmit,
-        handleInputChange,
-    }
+             inputs, handleSubmit, handleInputChange, setShowInvalidChild, showInvalidChild
+           }
 }
 
 export default useAddChild;
