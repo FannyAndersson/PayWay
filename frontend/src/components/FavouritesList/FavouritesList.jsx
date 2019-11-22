f1128aff7099e6d9b64d57e32791c3aa9185f994
Pablo44 <pawelrz@wp.pl>
2019-11-22 16:25:14 +0100

    new user context

--- frontend/src/components/FavouritesList/FavouritesList.jsx
+++ frontend/src/components/FavouritesList/FavouritesList.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { UserContext } from '../../UserContext';
+import React,{useContext} from 'react';
+import { UserContext } from '../../AuthUserContext';
 import { Row, Col, TextInput, Button } from 'react-materialize';
 
 const FavouritesList = () => {
@@ -14,9 +14,10 @@ const FavouritesList = () => {
                 <Col className='fav-list'>
                     <p className='fav-desc'>List of your favourites</p>
                     <ul className= 'fav-ul'>
-                        {user.favourites.map(fav => {
+                        {user ? user.favorites.map(fav => {
+                            console.log(fav)
                             return (<li className= 'fav-l-item' key={fav._id}>{fav.phone}</li>)
-                        })}
+                        }) : 'error'}
                     </ul>
                                         
                 </Col>
