ffd4f14db567afa1055e53d86ccdff8876de811d
EliasHjelm <elias.hj@gmail.com>
2019-12-10 10:28:41 +0100

    updated deployment script to be more communicative

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,21 @@
 echo "Starting automatic deployment... please take a deep breath :)"
 
 cd /var/www/PayWay
+echo "cd'ed into PayWay dir"
 git fetch
+echo "git fetched"
 git checkout master
+echo "checked out master"
 git pull
+echo "git pulled"
 npm install
+echo "npm installed in root dir"
 cd frontend
-npm install
+echo "cd'ed to frontend"
+npm 
+echo "npm installed in frontend"
 npm run build
+echo "Completed frontend production build"
 
 echo "Automatic deploy finished! Let's crack open a cold beer and enjoy this moment!"
 
