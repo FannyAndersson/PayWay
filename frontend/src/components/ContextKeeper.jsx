fece423310be5a8bedb28a589772e8faf89d60d0
Eugenia <demichka@gmail.com>
2019-12-16 13:56:24 +0100

    Fixed bug with no user.name in transaction in case of user was deleted from db

--- frontend/src/components/ContextKeeper.jsx
+++ frontend/src/components/ContextKeeper.jsx
@@ -51,7 +51,7 @@ const ContextKeeper = props => {
     return (
         <div>
             <Row>
-                <Col s={12} l={3} offset='l4'>
+                <Col s={12} m={6} l={4} xl={3} offset='xl4 l4 m2'>
                     {props.children}
                 </Col>
             </Row>
