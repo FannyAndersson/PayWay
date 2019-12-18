39d155dbb7d25dca8082b27b0a13782876db0bfd
EliasHjelm <elias.hj@gmail.com>
2019-12-18 10:32:48 +0100

    version 1.0

--- frontend/public/serviceWorker.js
+++ frontend/public/serviceWorker.js
@@ -1,5 +1,5 @@
 // this version number must be bumped when assets change - otherwise we will keep serving the old stuff from old cache
-const version = 0.3;
+const version = 1.0;
 
 // this gets magically changed to true in production
 const production = false;
