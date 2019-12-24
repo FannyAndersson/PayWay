d52ab2d32470705ad4b717e2c8375fa6639bde69
EliasHjelm <elias.hj@gmail.com>
2019-12-24 13:33:27 +0100

    bumped version 1.2.1

--- frontend/public/serviceWorker.js
+++ frontend/public/serviceWorker.js
@@ -1,5 +1,5 @@
 // this version number must be bumped when assets change - otherwise we will keep serving the old stuff from old cache
-const version = 1.2;
+const version = 1.21;
 
 // this gets magically changed to true in production
 const production = false;
